a2c9b5d35a3f51acdb7ee9c1fc9ce7d57b62962e
github username <github email address>
2020-06-11 12:45:09 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    # validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }, allow_nil: true
+     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+
+
+
+validate :content,
+
+    def content(clickbait)
+     clickbait = "Won't Believe"||"Secret"||"Top [number]"||"Guess"
+     if clickbait.present? && content.length < 250
+     else  errors.add(:content, "must contain clickbait Won't Believe, Secret,Top [number], or Guess")
+
+     end    
+    end
 end
